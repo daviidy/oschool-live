56fae9621ceb66addbcfd999a5627090ccfc0472
daviidy <yaodavidarmel@gmail.com>
2018-09-08 14:34:22 +0000

    espace formateurs

--- resources/views/layouts/registration.blade.php
+++ resources/views/layouts/registration.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="icons/image.png"/>
+	<link rel="icon" type="image/png" href="/avilon/img/favicon.png"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="/registration/vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
