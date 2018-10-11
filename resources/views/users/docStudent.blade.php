7af2ebf974c2e382bc9136dc196a65bce4349c66
daviidy <yaodavidarmel@gmail.com>
2018-10-11 16:09:20 +0000

    add project doc to studnat guide page

--- resources/views/users/docStudent.blade.php
+++ resources/views/users/docStudent.blade.php
@@ -21,6 +21,11 @@
           <h4 class="pull-left">Pour avoir une meilleure expérience d'apprentissage Oschool</h4>
           <a href="#" class="pull-right btn btn-theme02">+ Formez-vous avec Oschool</a>
           <div class="room-box">
+            <h5 class="text-primary"><a target="_blank" href="https://docs.google.com/document/d/1-xKgvHNuEIkalz4v0AUNT4dPZO-Z1-ixncv_uyzJv-0/edit?usp=sharing">Comment valider ses projets sur Oschool</a></h5>
+            <p>Ce guide vous explique comment se déroule la gestion des projets sur Oschool</p>
+            <p><span class="text-muted"> <a target="_blank" href="https://docs.google.com/document/d/1-xKgvHNuEIkalz4v0AUNT4dPZO-Z1-ixncv_uyzJv-0/edit?usp=sharing">Télécharger le guide</a> </span></p>
+          </div>
+          <div class="room-box">
             <h5 class="text-primary"><a target="_blank" href="https://oschool.ci/wp-content/uploads/2018/10/GuideEtudiantstlprsentiel.pdf">Guide étudiants du télé présentiel (A lire obligatoirement)</a></h5>
             <p>Il faut absolument lire ce guide pour savoir comment t'en sortir dans ta formation</p>
             <p><span class="text-muted"> <a target="_blank" href="https://oschool.ci/wp-content/uploads/2018/10/GuideEtudiantstlprsentiel.pdf">Télécharger le guide</a> </span></p>
