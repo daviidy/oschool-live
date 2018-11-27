6c3a7aed0d3e0c5c359b99d4c91035e7f46e237a
daviidy <yaodavidarmel@gmail.com>
2018-11-27 18:47:19 +0000

    arrangement parcours

--- resources/views/formations/ecoleprog.blade.php
+++ resources/views/formations/ecoleprog.blade.php
@@ -28,79 +28,7 @@
         <div class="language_title">Développez des compétences pour aujourd'hui, demain et le futur</div>
       </div>
     </div>
-    <div class="row">
-      <div class="col">
-        <div class="language_slider_container">
 
-          <!-- Language Slider -->
-
-          <div class="owl-carousel owl-theme language_slider">
-
-            <!-- Flag -->
-            <div class="owl-item language_item">
-              <a href="#">
-                <div class="flag"><img src="/new/images/icones/web-dev.png" alt=""></div>
-                <div class="lang_name">Ecole de programmation</div>
-              </a>
-            </div>
-
-
-            <!-- Flag -->
-            <div class="owl-item language_item">
-              <a href="#">
-                <div class="flag"><img src="/new/images/icones/photoshop.png" alt=""></div>
-                <div class="lang_name">Ecole du design</div>
-              </a>
-            </div>
-
-            <!-- Flag -->
-            <div class="owl-item language_item">
-              <a href="#">
-                <div class="flag"><img src="/new/images/icones/cm.png" alt=""></div>
-                <div class="lang_name">Ecole du Web Marketing</div>
-              </a>
-            </div>
-
-
-            <!-- Flag -->
-            <div class="owl-item language_item">
-              <a href="#">
-                <div class="flag"><img src="/new/images/icones/bourse.png" alt=""></div>
-                <div class="lang_name">Ecole du Business</div>
-              </a>
-            </div>
-
-            <!-- Flag -->
-            <div class="owl-item language_item">
-              <a href="#">
-                <div class="flag"><img src="https://cdn1.iconfinder.com/data/icons/world-flags-circular/1000/Flag_of_United_Kingdom_-_Circle-512.png" alt=""></div>
-                <div class="lang_name">Ecole des langues</div>
-              </a>
-            </div>
-
-            <!-- Flag -->
-            <div class="owl-item language_item">
-              <a href="#">
-                <div class="flag"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdjYVAAeHxJxhw1JZk_0m4E5PjMLv5VkxBrqCM-3Y6K9JrAFlr" alt=""></div>
-                <div class="lang_name">Ecole de la data science</div>
-              </a>
-            </div>
-
-            <!-- Flag -->
-            <div class="owl-item language_item">
-              <a href="#">
-                <div class="flag"><img src="https://cdn2.vectorstock.com/i/1000x1000/52/41/artificial-intelligence-brain-icon-ai-vector-21645241.jpg" alt=""></div>
-                <div class="lang_name">Ecole de l'intelligence Artificielle</div>
-              </a>
-            </div>
-
-          </div>
-
-          <div class="lang_nav lang_prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>
-          <div class="lang_nav lang_next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>
-        </div>
-      </div>
-    </div>
   </div>
 </div>
 
@@ -116,22 +44,39 @@
         <div class="course">
           <div class="course_image"><img src="/new/images/cours/devweb.png" alt=""></div>
           <div class="course_body">
-            <div class="course_title"><a href="/devweb">Développeur web</a></div>
-            <div class="course_info">
-              <ul>
-                <li><a href="#">Programmation et développement</a></li>
-                <li><a href="#">4 mois</a></li>
-              </ul>
-            </div>
-            <div class="course_text">
-              <p>Faites vos premiers pas dans le monde du développement web : créez des sites web et donnez vie aux maquettes !</p>
-            </div>
+            <div class="course_title"><a href="/devweb">Développeur Web Junior</a></div>
+
+
           </div>
-          <div class="course_footer d-flex flex-row align-items-center justify-content-start">
-            <div class="course_students"><i class="fa fa-user" aria-hidden="true"></i><span>20</span></div>
-            <div class="course_rating ml-auto"><i class="fa fa-star" aria-hidden="true"></i><span>4,5</span></div>
-            <div class="course_mark course_free trans_200"><a href="#">Certifié</a></div>
+
+        </div>
+      </div>
+
+      <!-- Course -->
+
+      <div class="col-lg-4 course_col">
+        <div class="course">
+          <div class="course_image"><img src="/new/images/cours/devweb3.png" alt=""></div>
+          <div class="course_body">
+            <div class="course_title"><a href="#">Développeur Web Front-End</a></div>
+
+
           </div>
+
+        </div>
+      </div>
+
+      <!-- Course -->
+
+      <div class="col-lg-4 course_col">
+        <div class="course">
+          <div class="course_image"><img src="/new/images/cours/devweb2.png" alt=""></div>
+          <div class="course_body">
+            <div class="course_title"><a href="#">Développeur Web Full Stack</a></div>
+
+
+          </div>
+
         </div>
       </div>
 
@@ -164,29 +109,24 @@
       <!-- Course -->
       <div class="col-lg-4 course_col">
         <div class="course">
-          <div class="course_image"><img src="/new/images/cours/android.png" alt=""></div>
+          <div class="course_image"><img src="/new/images/cours/android2.png" alt=""></div>
           <div class="course_body">
             <div class="course_title"><a href="{{url('android')}}">Développeur d'Applications Android</a></div>
-            <div class="course_info">
-              <ul>
-                <li><a href="#">Programmation et développement</a></li>
-                <li><a href="#">8 mois</a></li>
-              </ul>
-            </div>
-            <div class="course_text">
-              <p>Une série de cours gratuits, en ligne et à votre rythme,
-                destinés aux développeurs Android débutants sans expérience en programmation.
-                Dans le premier cours, vous construisez votre première application Android.
-                Les derniers cours de la série vous apprendront comment ajouter plus de fonctionnalités
-                à votre application, vous connecter à Internet,
-                configurer des bases de données et stocker des données dans votre application.</p>
-            </div>
+
           </div>
-          <div class="course_footer d-flex flex-row align-items-center justify-content-start">
-            <div class="course_students"><i class="fa fa-user" aria-hidden="true"></i><span>10</span></div>
-            <div class="course_rating ml-auto"><i class="fa fa-star" aria-hidden="true"></i><span>4,5</span></div>
-            <div class="course_mark course_free trans_200"><a href="#">Certifié</a></div>
+
+        </div>
+      </div>
+
+      <!-- Course -->
+      <div class="col-lg-4 course_col">
+        <div class="course">
+          <div class="course_image"><img src="/new/images/cours/android3.png" alt=""></div>
+          <div class="course_body">
+            <div class="course_title"><a href="#">Les bases d'Android</a></div>
+
           </div>
+
         </div>
       </div>
 
@@ -245,21 +185,9 @@
           <div class="course_image"><img src="/new/images/cours/jeux.png" alt=""></div>
           <div class="course_body">
             <div class="course_title"><a href="{{url('jeuxvideos')}}">Développeur de jeux vidéos</a></div>
-            <div class="course_info">
-              <ul>
-                <li><a href="#">Programmation et développement</a></li>
-                <li><a href="#">4 mois</a></li>
-              </ul>
-            </div>
-            <div class="course_text">
-              <p>Découvrez l'univers des jeux vidéos, et développez vos propres jeux avec Unity</p>
-            </div>
+
           </div>
-          <div class="course_footer d-flex flex-row align-items-center justify-content-start">
-            <div class="course_students"><i class="fa fa-user" aria-hidden="true"></i><span>5</span></div>
-            <div class="course_rating ml-auto"><i class="fa fa-star" aria-hidden="true"></i><span>4,5</span></div>
-            <div class="course_mark course_free trans_200"><a href="#">Certifié</a></div>
-          </div>
+
         </div>
       </div>
 
