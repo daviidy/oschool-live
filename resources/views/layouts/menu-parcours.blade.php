1ef696858227ff257e358b96ff43e6087f1da76c
daviidy <yaodavidarmel@gmail.com>
2019-01-12 15:05:07 +0000

    creation page de vente facebook marketing

--- resources/views/layouts/menu-parcours.blade.php
+++ resources/views/layouts/menu-parcours.blade.php
@@ -86,7 +86,7 @@ drift.load('2uy6g3spxi59');
 
 <script>
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 06, 2019 23:59:59").getTime();
+var countDownDatePath = new Date("Jan 13, 2019 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -95,7 +95,7 @@ var x = setInterval(function() {
   var now = new Date().getTime();
 
   // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  var distance = countDownDatePath - now;
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
