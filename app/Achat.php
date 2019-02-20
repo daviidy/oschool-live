ae7e8df33354d01eb9bbeca035f09a74e7a40c76
daviidy <yaodavidarmel@gmail.com>
2019-02-20 17:10:41 +0000

    test cinetpay 12

--- app/Achat.php
+++ app/Achat.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Achat extends Model
 {
-    protected $fillable = ['email', 'nom', 'prenoms', 'tel', 'user_id', 'montant', 'formation', 'date', 'promo', 'trans_id', 'signature', 'statut'];
+    protected $fillable = ['email', 'nom', 'prenoms', 'tel', 'user_id', 'montant', 'formation', 'date', 'promo', 'trans_id', 'signature', 'statut', 'achat_id'];
 
     public function student()
     {
