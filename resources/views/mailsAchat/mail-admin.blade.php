3c45b76aca911b1371f9c1c6f9e2d9ccd8b0eef4
daviidy <yaodavidarmel@gmail.com>
2019-02-20 16:34:50 +0000

    test cinetpay 11

--- resources/views/mailsAchat/mail-admin.blade.php
+++ resources/views/mailsAchat/mail-admin.blade.php
@@ -31,9 +31,9 @@
       <td style="padding: 20px 7.5% 0;"> Voici les infos de la transaction  <br>
 				<ul>
 					<li>Plateforme: <strong>Parcours Oschool</strong> </li>
-					<li>Utilisateur: <strong> <a href="{{url('users', $achat->user_id)}}">{{$achat->prenoms}} {{$achat->nom}}</a> </strong> </li>
-					<li>Formation choisie: <strong>{{$achat->formation}}</strong> </li>
-					<li>Date de paiement: <strong>{{$achat->date}}</strong> </li>
+					<li>Utilisateur: <strong> <a href="{{url('users', $commande->user_id)}}">{{$commande->prenoms}} {{$commande->nom}}</a> </strong> </li>
+					<li>Formation choisie: <strong>{{$commande->formation}}</strong> </li>
+					<li>Date de paiement: <strong>{{$commande->date}}</strong> </li>
 				</ul>
 
       </td>
