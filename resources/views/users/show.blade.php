35dd4cea473947411e8fe7fb1a2ee89a0e2509ca
daviidy <yaodavidarmel@gmail.com>
2019-03-21 00:32:29 +0000

    paypal dispo 7

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -435,7 +435,7 @@
                            </tr>
                          </thead>
                          <tbody>
-                           @foreach($user->sessions->sortBy('date') as $classroom)
+                           @foreach($user->sessions->sortByDesc('date') as $classroom)
                            <tr>
                              <td><a href="{{url('users', $classroom->idEtudiant)}}">{{$classroom->etudiant}}</a></td>
                              <td> <a data-toggle="modal" data-target="#myModal{{$classroom->id}}" href="#">{{ Carbon\Carbon::parse($classroom->date)->format('d-m-Y H:i') }}: voir le compte-rendu</a></td>
